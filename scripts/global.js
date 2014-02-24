9eb08160dd6f41343dc8e3671b24d73e7ea5f07e
Lachlan C <lachlanjc@users.noreply.github.com>
2014-02-24 15:12:35 -0500

    Create global.js

---
+++ scripts/global.js
@@ -0,0 +1,12 @@
+function triggeralert() {
+    alert("Sorry, this feature does not work yet, like the rest of the app. It is in active development, but you still might need to wait a few thousand years.");
+}
+function linky() {
+    alert("Doesn't work yet, sorry! Please wait a few thousand years.");
+}
+function newquiz() {
+    window.location("https://c9.io/lachlanjc/le-pfms/workspace/quiz/pages/newquiz.html");
+}
+function backtotd() {
+    window.location("https://c9.io/lachlanjc/le-pfms/workspace/quiz/pages/teacher-d.html");
+}
