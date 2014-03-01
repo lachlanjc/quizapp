724c08cf23ac1f991aea1a6d8e09035b11a86c39
tns12 <tns12@users.noreply.github.com>
2014-03-01 07:52:43 -0500

    Added backToDashboard()

--- scripts/takequiz.js
+++ scripts/takequiz.js
@@ -1,3 +1,6 @@
 function confirm(){
     alert("Are you sure you want to submit your quiz? You cannot change your answers after it has been submitted.")
 }
+function backToDashboard(){
+    window.location("http://pfmscode.github.io/quizapp/pages/student-d.html")
+}
