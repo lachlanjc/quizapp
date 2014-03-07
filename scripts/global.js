767489b1eb7eabd89b2d50a917b7f43729493d3f
KeddyKed <wlh12@scasd.org>
2014-03-07 14:53:15 -0500

    Added sub-joke

--- scripts/global.js
+++ scripts/global.js
@@ -2,7 +2,7 @@ function triggeralert() {
     alert("Sorry, this feature does not work yet, like the rest of the app. It is in active development, but you still might need to wait a few thousand years.");
 }
 function linky() {
-    alert("Doesn't work yet, sorry! Please wait a few thousand years.");
+    alert("Doesn't work yet, sorry! Please wait a few thousand years. It might seem like a long time, but time will go by in the blink of an eye!");
 }
 function newquiz() {
     window.location("http://pfmscode.github.io/quizapp/pages/newquiz.html");
