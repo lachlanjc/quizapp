4ef2dc3ab07dce962eb2ce3b6fc70cb31ee6edac
KeddyKed <wlh12@scasd.org>
2014-03-07 14:45:59 -0500

    I changed "it has" to "they have"

--- scripts/takequiz.js
+++ scripts/takequiz.js
@@ -1,5 +1,5 @@
 function confirm(){
-    alert("Are you sure you want to submit your quiz? You cannot change your answers after it has been submitted.")
+    alert("Are you sure you want to submit your quiz? You cannot change your answers after they have been submitted.")
 }
 function backToDashboard(){
     window.location("http://pfmscode.github.io/quizapp/pages/student-d.html")
